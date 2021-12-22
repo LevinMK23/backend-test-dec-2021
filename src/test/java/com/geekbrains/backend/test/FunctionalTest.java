9fc1b1121d5ffdfe6c5c6d61ffa70424e9a45b67
Михаил Левин <mikelevin@yandex-team.ru>
2021-12-22 20:56:57 +0300

    Lesson 7. Jenkins

--- src/test/java/com/geekbrains/backend/test/FunctionalTest.java
+++ src/test/java/com/geekbrains/backend/test/FunctionalTest.java
@@ -2,10 +2,9 @@ package com.geekbrains.backend.test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public abstract class FunctionalTest {
@@ -13,8 +12,8 @@ public abstract class FunctionalTest {
 
     public static Properties readProperties() throws Exception {
         Properties properties = new Properties();
-        Files.readAllLines(Path.of(
-                        FunctionalTest.class.getResource("test.properties").toURI()))
+        Files.readAllLines(Paths.get(
+                FunctionalTest.class.getResource("test.properties").toURI()))
                 .forEach(str -> {
                     String[] props = str.split("=");
                     properties.setProperty(props[0].trim(), props[1].trim());
@@ -24,9 +23,13 @@ public abstract class FunctionalTest {
 
     public String getStringResource(String name) throws IOException {
         String dir = getClass().getSimpleName();
-        byte[] bytes = getClass().getResourceAsStream(dir + "/" + name)
-                .readAllBytes();
-        return new String(bytes, StandardCharsets.UTF_8);
+        InputStream is = getClass().getResource(dir + "/" + name)
+                .openStream();
+        StringBuilder s = new StringBuilder();
+        while (is.available() > 0) {
+            s.append((char) is.read());
+        }
+        return s.toString();
     }
 
     public File getFileResource(String name) {
